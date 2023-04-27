2e95a9a3111dc83bb2647c3da47ea33d7abb13af
Artem <ba.pydev@gmail.com>
2023-04-27 12:20:04 +0300

    Фиск миграций

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -99,7 +99,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.getenv('redis', "127.0.0.1"), 6379)],
+            "hosts": [(os.getenv('REDIS_HOST', "127.0.0.1"), 6379)],
         },
     },
 }
