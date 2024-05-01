1e8f0a6a779f01619e015b5526f62851cf752708
Artem <ba.pydev@gmail.com>
2024-05-01 17:24:04 +0400

    Init order status script add

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,29 +1,15 @@
-import os
-
-from django.conf import settings
-
-from core.models import Fabric, ProductPicture
+from core.models import Order, OrderProduct
 
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
-    thumbnails_dir = os.path.join(settings.MEDIA_ROOT, 'images/products/thumbnails/')
-    print(thumbnails_dir)
-    thumbnails_files = set(os.listdir(thumbnails_dir))
-    print(thumbnails_files)
+    orders_sq = Order.objects.all()
 
-    # Получаем список имен файлов из объектов модели
-    used_filenames = set()
-    for model in [Fabric, ProductPicture]:
-        for obj in model.objects.all():
-            if obj.thumbnail:
-                used_filenames.add(os.path.basename(obj.thumbnail.name))
-
-    # Определяем файлы, которые не используются и можно удалить
-    unused_files = thumbnails_files - used_filenames
-
-    for filename in unused_files:
-        file_path = os.path.join(thumbnails_dir, filename)
-        os.remove(file_path)
-        print(f"Deleted unused thumbnail: {filename}")
+    for order in orders_sq:
+        order_products_active_exists = OrderProduct.objects.filter(
+            status="0"
+        ).exists()
+        if not order_products_active_exists:
+            order.status = "1"
+            order.save()
