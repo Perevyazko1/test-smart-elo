787e836b0a0182f459c65561990ae80b3d0f8b87
Artem <ba.pydev@gmail.com>
2024-06-07 14:29:58 +0700

    fix static admin

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -1,12 +1,11 @@
 """Project urls. """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import path, include, re_path
-from django.views.static import serve
+from django.urls import path, include
 from rest_framework.decorators import api_view
 
-from django.conf.urls.static import static
 from django_app.init_data.init_data import init_data
 
 
@@ -24,11 +23,11 @@ urlpatterns = [
     path('api/v1/staff/', include('staff.urls')),
 
     path('init/', init_app_data),
-
-    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT,
+    )
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(
