13b555fcd76e731015a808155591a31c247c1f1f
Artem <ba.pydev@gmail.com>
2024-06-24 23:22:46 +0700

    asgi fix

--- django_app/django_app/asgi.py
+++ django_app/django_app/asgi.py
@@ -1,4 +1,3 @@
-
 import os
 
 from channels.auth import AuthMiddlewareStack
@@ -6,12 +5,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
+
+# Загрузите все необходимые модули перед инициализацией Django приложения
+import django
+django.setup()
+
 from core.routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_app.settings")
 django_asgi_app = get_asgi_application()
-
-import core.routing
 
 application = ProtocolTypeRouter(
     {
