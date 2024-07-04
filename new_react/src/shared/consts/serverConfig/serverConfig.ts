98bc841d8cb72ff3968f181e3b10f7115bb00278
Artem <ba.pydev@gmail.com>
2024-07-04 21:51:03 +0700

    Nginx

--- new_react/src/shared/consts/serverConfig/serverConfig.ts
+++ new_react/src/shared/consts/serverConfig/serverConfig.ts
@@ -1,10 +1,11 @@
 export const SERVER_HOST = `https://${window.location.hostname}`;
 export const WS_HOST = `ws://${window.location.hostname}`;
-export const SERVER_PORT = '8000';
+export const SERVER_PORT = '3000';
 
 export const GET_STATIC_URL = () => {
     return `http://${window.location.hostname}`
 }
 
-export const SERVER_HTTP_ADDRESS = `${SERVER_HOST}:${SERVER_PORT}`
+// export const SERVER_HTTP_ADDRESS = `${SERVER_HOST}:${SERVER_PORT}`
+export const SERVER_HTTP_ADDRESS = `elo.szmk.pro`
 export const SERVER_WS_ADDRESS = `${WS_HOST}:${SERVER_PORT}`
