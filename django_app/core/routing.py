a7663b61ec0ad9dd4fa6e4fa0257f8f14ea9b37f
Artem <ba.pydev@gmail.com>
2024-07-04 23:00:43 +0700

    Nginx

--- django_app/core/routing.py
+++ django_app/core/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"wss/(?P<pin_code>\w+)/(?P<department_number>\w+)/$", consumers.MainConsumer.as_asgi()),
+    re_path(r"ws/(?P<pin_code>\w+)/(?P<department_number>\w+)/$", consumers.MainConsumer.as_asgi()),
 ]
