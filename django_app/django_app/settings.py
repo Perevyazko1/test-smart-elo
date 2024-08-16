9a9f5cdded92e722fcf46c72c27e395a8889d5f2
Artem <ba.pydev@gmail.com>
2024-08-16 23:05:57 +0800

    Redis

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -180,7 +180,7 @@ CELERY_CACHE_BACKEND = 'default'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
