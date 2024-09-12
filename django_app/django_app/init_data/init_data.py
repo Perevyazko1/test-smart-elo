2b4a2ea3b7c606085baee797136d0ba31bc87b23
Artem <ba.pydev@gmail.com>
2024-09-12 23:46:08 +0800

    целери бит Фикс

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,10 +1,9 @@
 """Initial methods and scripts. """
-from tasks.models import Task, TaskExecutor
-from django_celery_beat.models import PeriodicTask, PeriodicTasks
+from django_celery_beat.models import PeriodicTask, PeriodicTasks, now
 
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
     PeriodicTask.objects.update(last_run_at=None)
-    PeriodicTasks.objects.changed()
+    PeriodicTasks.objects.all().update_or_create(ident=1, defaults={'last_update': now()})
