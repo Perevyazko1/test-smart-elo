51681d0bdee0387cae1f138b4873ad7e5cd00e4e
Artem <ba.pydev@gmail.com>
2024-09-13 15:54:12 +0800

    скрипт для фикса нарядов без тариффа

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,9 +1,20 @@
 """Initial methods and scripts. """
-from django_celery_beat.models import PeriodicTask, PeriodicTasks, now
+from core.models import Assignment, ProductionStep, OrderProduct
 
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
-    PeriodicTask.objects.update(last_run_at=None)
-    PeriodicTasks.update_changed()
+    target_production_steps = ProductionStep.objects.filter(
+        confirmed_tariff__isnull=False
+    )
+    for production_step in target_production_steps:
+        Assignment.objects.filter(
+            order_product__product=production_step.product,
+            new_tariff__isnull=True,
+            department=production_step.department,
+        ).update(
+            new_tariff=production_step.confirmed_tariff,
+            amount=production_step.confirmed_tariff.amount,
+        )
+
