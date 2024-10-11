1133d4dbf3bae99e6ebbe1989f47486c61c62e20
Artem <ba.pydev@gmail.com>
2024-10-11 13:11:51 +0300

    Скрипт для фикса нарядов {8}24322

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,17 +1,24 @@
 """Initial methods and scripts. """
-from core.models import Assignment
+from core.models import Assignment, OrderProduct
+from staff.models import Department
+from core.signals import update_assignments_and_clean_cache
 
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
+
+    department = Department.objects.get(name="Обивка")
+    order_product = OrderProduct.objects.get(series_id="{8}24322")
+
     target_assignments = Assignment.objects.filter(
-        new_tariff__isnull=False,
-        inspector__isnull=False,
-        inspect_date__isnull=False,
-        tariffication_date__isnull=True,
+        order_product=order_product,
+        department=department
     )
 
-    for assignment in target_assignments:
-        assignment.tariffication_date = assignment.inspect_date
-        assignment.save()
+    update_assignments_and_clean_cache(
+        target_assignments,
+        order_product.id,
+        department.id,
+        assembled=True,
+    )
