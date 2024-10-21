0c2a50eb1003b3efe8073a974b7ca2ba2dfe8e97
Artem <ba.pydev@gmail.com>
2024-10-21 19:55:18 +0300

    Статика для админки

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -31,8 +31,7 @@ urlpatterns += static(
     document_root=settings.STATIC_ROOT,
 )
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
