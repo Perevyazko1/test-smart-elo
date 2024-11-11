82e705ceb74be3a337479600ad5dcde3d2552347
Artem <ba.pydev@gmail.com>
2024-11-11 09:17:18 +0300

    Скрипт для исправления нулевых карточек

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,29 +1,20 @@
 """Initial methods and scripts. """
-from core.models import OrderProduct, ProductionStep, Assignment
+from core.models import Assignment, AssignmentCoExecutor
 from core.services.assignment_generator import AssignmentGenerator
 
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
-
-    current_op = OrderProduct.objects.filter(
-        status="0"
+    target_assignments = Assignment.objects.filter(
+        status='in_work',
+        department__piecework_wages=True,
     )
-
-    for op in current_op:
-        target_ps = ProductionStep.objects.filter(
-            product=op.product,
-            is_active=True,
-        ).exclude(
-            department__number__in=[0, 1, 50]
-        )
-        for ps in target_ps:
-            has_assignments = Assignment.objects.filter(
-                department=ps.department,
-                order_product=op
-            ).exists()
-
-            if not has_assignments:
-                print(f"Отдел {ps.department.name} серии {op.series_id} без нарядов!")
-                AssignmentGenerator().init_order_product_assignments(op)
+    for assignment in target_assignments:
+        if assignment.new_tariff:
+            if AssignmentCoExecutor.objects.filter(
+                assignment=assignment
+            ).exists():
+                continue
+            else:
+                assignment.amount = assignment.new_tariff.amount
