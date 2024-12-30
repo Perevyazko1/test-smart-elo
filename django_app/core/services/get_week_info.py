dce16859a6d89023451eb660a2992d7d36367c91
Artem <ba.pydev@gmail.com>
2024-12-30 17:12:45 +0400

    Исправлено вычисление недели

--- django_app/core/services/get_week_info.py
+++ django_app/core/services/get_week_info.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 class WeekInfo:
     week: str
     year: str
-    str_dates: [str]
+    str_dates: list[str]
     dt_dates: list[datetime]
     date_range: list[datetime]
     previous_week_data: dict
@@ -22,11 +22,13 @@ class GetWeekInfo:
         self.year = year
 
     def _get_initial_values(self):
+        iso_year, iso_week, _ = datetime.today().isocalendar()
+
         if not self.week or not self.week.isdigit():
-            self.week = str(datetime.today().isocalendar()[1])
+            self.week = str(iso_week)
 
         if not self.year or not self.year.isdigit():
-            self.year = str(datetime.now().year)
+            self.year = str(iso_year)
 
     def execute(self) -> WeekInfo:
         self._get_initial_values()
@@ -37,20 +39,22 @@ class GetWeekInfo:
         str_dates = []
         dt_dates = []
         for day in range(7):
-            str_dates.append((first_day + timedelta(days=day)).strftime('%d.%m'))
-            dt_dates.append(first_day + timedelta(days=day))
+            current_day = first_day + timedelta(days=day)
+            str_dates.append(current_day.strftime('%d.%m'))
+            dt_dates.append(current_day)
 
         range_first = dt_dates[0]
         range_last = dt_dates[-1]
         date_range = [range_first, range_last]
 
-        prev_week = (first_day - timedelta(days=7)).isocalendar()[1]
-        prev_week_year = (first_day - timedelta(days=7)).isocalendar()[0]
-        next_week = (first_day + timedelta(days=7)).isocalendar()[1]
-        next_week_year = (first_day + timedelta(days=7)).isocalendar()[0]
+        prev_date = first_day - timedelta(days=7)
+        prev_year, prev_week, _ = prev_date.isocalendar()
 
-        previous_week_data = {"week": prev_week, "year": prev_week_year}
-        next_week_data = {"week": next_week, "year": next_week_year}
+        next_date = first_day + timedelta(days=7)
+        next_year, next_week, _ = next_date.isocalendar()
+
+        previous_week_data = {"week": str(prev_week), "year": str(prev_year)}
+        next_week_data = {"week": str(next_week), "year": str(next_year)}
 
         return WeekInfo(
             week=self.week,
