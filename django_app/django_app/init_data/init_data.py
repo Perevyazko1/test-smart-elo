458edd09a098c4e3094692cec9fe1c619bd6cc74
Artem <ba.pydev@gmail.com>
2025-01-07 10:46:13 +0400

    Фикс нарядов без даты закрепления

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,79 +1,29 @@
 """Initial methods and scripts. """
 from datetime import datetime, timedelta
-import re
 
-from django.core.exceptions import ObjectDoesNotExist
-
-from core.models import AssignmentCoExecutor, Assignment
-from staff.models import Department, Transaction
-
-
-def find_assignment_by_string(input_string):
-    # Регулярное выражение для извлечения данных
-    pattern = r'№(\d+) - \{(\d+)\}(\d+)\s+(\w+)'
-    match = re.search(pattern, input_string)
-
-    if not match:
-        return None  # Если строка не соответствует формату
-
-    # Извлечение данных из строки
-    assignment_number = int(match.group(1))
-    series_prefix = match.group(2)
-    series_id = match.group(3)
-    department_name = match.group(4)
-
-    # Полный series_id (можно уточнить формат, если есть префиксы)
-    full_series_id = f"{{{series_prefix}}}{series_id}"
-
-    try:
-        # Поиск отдела
-        department = Department.objects.get(name=department_name)
-
-        # Поиск наряда
-        assignment = Assignment.objects.get(
-            number=assignment_number,
-            order_product__series_id=full_series_id,
-            department=department
-        )
-
-        return assignment  # Возвращаем найденный объект Assignment
-
-    except ObjectDoesNotExist:
-        return None
+from core.models import Assignment
+from staff.models import Department
 
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
     # TODO скрипт за последний месяц пересчитать наряды по сдельщикам которые были на окладе
-    result = {}
     today = datetime.now()
     target_month = today - timedelta(days=30)
-
-    target_transactions = Transaction.objects.filter(
-        add_date__gte=target_month,
-        inspect_date__isnull=True,
-        inspector__isnull=False
+    target_departments = Department.objects.filter(
+        piecework_wages=False
     )
 
-    for transaction in target_transactions:
-        assignment = find_assignment_by_string(transaction.description)
+    for department in target_departments:
+        target_assignments = Assignment.objects.filter(
+            department=department,
+            inspector__isnull=False,
+            tariffication_date__isnull=True
+        )
+        print(target_departments.count())
+        for assignment in target_assignments:
+            assignment.tariffication_date = assignment.inspect_date
+            assignment.save()
 
-        if assignment:
-            result[f'id{transaction.id}'] = f"Наряд найден: {assignment}"
-
-            if assignment.executor.piecework_wages:
-                co_executors = AssignmentCoExecutor.objects.filter(
-                    assignment=assignment
-                )
-                difference = 0
-                for co_executor in co_executors:
-                    difference += co_executor.amount
-
-                transaction.amount = assignment.new_tariff.amount - difference
-                transaction.inspect_date = transaction.add_date
-                transaction.save()
-        else:
-            result[f'id{transaction.id}'] = f"Наряд не найден {transaction.description}"
-
-    return result
+    return target_month
