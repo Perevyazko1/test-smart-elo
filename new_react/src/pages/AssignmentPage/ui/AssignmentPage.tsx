4e7522c0a589f1fdc82715b5d32964cf5ba99b34
Artem <ba.pydev@gmail.com>
2025-02-21 11:23:23 +0400

    Счетчик нарядов

--- new_react/src/pages/AssignmentPage/ui/AssignmentPage.tsx
+++ new_react/src/pages/AssignmentPage/ui/AssignmentPage.tsx
@@ -114,7 +114,11 @@ export const AssignmentPage = () => {
                                 <div className="d-flex align-items-center p-2">
                                     <h4 className={'m-0'}>
                                         Страница работы с нарядами
-                                        {assignmentProps.isLoading && <Spinner animation={'grow'} size={'sm'}/>}
+                                        {assignmentProps.isLoading ?
+                                            <Spinner animation={'grow'} size={'sm'}/>
+                                            :
+                                            <>{assignmentProps.count}</>
+                                        }
                                     </h4>
                                 </div>
                                 <hr className={'p-0 me-2 mx-2 mt-0 w-25'}/>
