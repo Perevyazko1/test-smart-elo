959a293e93157ebfd671720ad18793c9b057e68b
Artem <ba.pydev@gmail.com>
2025-02-27 12:52:31 +0400

    скрипт

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,5 +1,5 @@
 """Initial methods and scripts."""
-from core.models import OrderProduct
+from core.models import OrderProduct, Assignment
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
@@ -7,6 +7,9 @@ def init_data():
     target_op = OrderProduct.objects.get(
         series_id="{3}24856"
     )
-    target_op.assignments.all().delete()
+    target_assignments = Assignment.objects.filter(
+        order_product=target_op
+    )
+    target_assignments.delete()
     target_op.delete()
     return "OK"
