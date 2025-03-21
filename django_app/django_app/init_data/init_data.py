c5186b9fa3daa39c9fc80602123f52d982148c08
Artem <ba.pydev@gmail.com>
2025-03-21 16:59:55 +0300

    Убрана инициализация

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,16 +1,8 @@
 """Initial methods and scripts."""
-from core.models import OrderProduct
-import re
 
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
-    target_op = OrderProduct.objects.all()
-
-    for op in target_op:
-        result = re.sub(r'^\{(\d+)\}(.*)$', r'\1-\2', op.series_id)
-        op.series_id = result
-        op.save()
 
     return "Ok"
