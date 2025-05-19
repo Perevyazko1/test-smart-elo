e281d05b6a33aaf34b6ba5d147fb64e48972e696
bapyd <ba.pydev@gmail.com>
2025-05-19 15:21:44 +0300

    исправление сделанных нарядов

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,17 +1,47 @@
 """Initial methods and scripts."""
-from core.models import ProductionStep
-from staff.models import Department
+from core.models import Assignment
+from staff.models import Department, Employee, Transaction
+from tasks.models import Task
 
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
-    for ps in ProductionStep.objects.all():
-        next_ps = ps.next_step.all()
-        next_departments = Department.objects.filter(productionstep__in=next_ps)
+    TARGET_DATE = "2025-05-19"
+    NEW_DATE = "2025-05-16"
+    target_department = Department.objects.get(name="Сборка")
 
-        for department in next_departments:
-            ps.next_steps.add(department.id)
-        ps.save()
+    target_users=Employee.objects.filter(
+        departments=target_department.id,
+    )
+
+    target_assignments = Assignment.objects.filter(
+        tariffication_date__date=TARGET_DATE,
+        department=target_department,
+    )
+
+    target_assignments.update(
+        tariffication_date=NEW_DATE,
+    )
+
+    target_tasks = Task.objects.filter(
+        verified_at__date=TARGET_DATE,
+        new_executor__employee__in=target_users,
+    )
+
+    target_tasks.update(
+        verified_at=NEW_DATE,
+    )
+
+    target_payments = Transaction.objects.filter(
+        target_date__date=TARGET_DATE,
+        employee__in=target_users,
+    )
+
+    target_payments.update(
+        target_date=NEW_DATE,
+    )
+
+
     print('PASS')
-    return "Oki"
+    return f"Oki {target_assignments.count()} {target_tasks.count()} {target_users.count()} {target_payments.count()}"
