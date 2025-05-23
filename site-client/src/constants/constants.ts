ebd2147c76108d5660fb227d42983e1f13b04127
bapyd <ba.pydev@gmail.com>
2025-05-23 15:31:42 +0300

    Конфиг докера

--- site-client/src/constants/constants.ts
+++ site-client/src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL = "http://backend:8000";
-export const API_URL = "http://backend:8000/api";
+export const SERVER_URL = "http://site-backend:8000";
+export const API_URL = "http://site-backend:8000/api";
 export const IS_CLIENT = typeof window !== "undefined";
