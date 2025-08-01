7569e89eb0489270b602d5b1ee627d9bcd50880e
bapyd <ba.pydev@gmail.com>
2025-08-01 09:40:25 +0300

    Скрипт фикс

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,19 +1,24 @@
 """Initial methods and scripts."""
 from datetime import timedelta
 
-from salary.models import Earning
+from salary.models import PayrollRow
 
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
-    date_to = "2025-07-20"
-    target_earnings = Earning.objects.filter(
-        target_date__lte=date_to,
-    )
+    # date_to = "2025-07-20"
+    # target_earnings = Earning.objects.filter(
+    #     target_date__lte=date_to,
+    # )
 
-    for earning in target_earnings:
+    all_rows = PayrollRow.objects.all()
+
+    for row in all_rows:
+        row.cash_payout = row.cash_payout * 100
+
+    # for earning in target_earnings:
         # earning.cash_date = earning.target_date + timedelta(days=7)
-        earning.save()
+        # earning.save()
 
     print('PASS')
     return f"Oki"
