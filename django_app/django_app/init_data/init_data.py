5f9a416381ebd758307d8a075d9a4402bdad0cf9
bapyd <ba.pydev@gmail.com>
2025-08-04 09:37:02 +0300

    Отключение скрипта

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -6,15 +6,15 @@ from salary.models import Earning
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
-    date_to = "2025-08-01"
-    target_earnings = Earning.objects.filter(
-        target_date__gte=date_to,
-        earning_type="ЭЛО",
-    )
-
-    for earning in target_earnings:
-        earning.amount = earning.amount * 100
-        earning.save()
+    # date_to = "2025-08-01"
+    # target_earnings = Earning.objects.filter(
+    #     target_date__gte=date_to,
+    #     earning_type="ЭЛО",
+    # )
+    #
+    # for earning in target_earnings:
+    #     earning.amount = earning.amount * 100
+    #     earning.save()
 
     print('PASS')
     return f"Oki"
