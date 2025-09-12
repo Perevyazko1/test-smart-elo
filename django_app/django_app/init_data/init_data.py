d2ba5b737087ca5b607f5ee41e86d18c0ee8093c
bapyd <ba.pydev@gmail.com>
2025-09-12 17:35:39 +0300

    Переделан импорт заказов

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,8 +1,7 @@
 """Initial methods and scripts."""
 import logging
 
-from core.models import OrderProduct, Assignment, Order
-from staff.models import Department
+from core.models import OrderProduct, Order
 
 logger = logging.getLogger(__name__)
 
@@ -10,23 +9,6 @@ logger = logging.getLogger(__name__)
 def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
-    active_order_positions = OrderProduct.objects.filter(status="0")
-
-    for order_product in active_order_positions:
-        if order_product.assignments.exclude(status="ready").exists():
-            Assignment.objects.filter(
-                order_product=order_product,
-            ).update(
-                sort_date=order_product.order.planned_date
-            )
-            continue
-        print(f"{order_product.series_id} все наряды закрыты")
-        order_product.status = "1"
-        order_product.save()
-        if order_product.order.status == "0":
-            order_product.order.status = "1"
-            order_product.order.save()
-
     orders = Order.objects.filter(status="0")
 
     for order in orders:
