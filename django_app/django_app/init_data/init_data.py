1694d91bcd0109c59033e8bbb13c5bd00571401f
bapyd <ba.pydev@gmail.com>
2025-12-01 13:54:26 +0300

    Убран скрипт работы с датой плановой

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,7 +1,7 @@
 """Initial methods and scripts."""
 import logging
 
-from core.models import OrderProduct, Assignment
+# from core.models import OrderProduct, Assignment
 from django_app.init_data.reports.finance import get_finance_report
 
 logger = logging.getLogger(__name__)
@@ -12,12 +12,12 @@ def init_data():
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
 
     get_finance_report()
-
-    active_order_products = OrderProduct.objects.filter(status="0")
-
-    for order_product in active_order_products:
-        assignments = Assignment.objects.filter(order_product=order_product)
-        assignments.update(sort_date=order_product.order.planned_date)
+    #
+    # active_order_products = OrderProduct.objects.filter(status="0")
+    #
+    # for order_product in active_order_products:
+    #     assignments = Assignment.objects.filter(order_product=order_product)
+    #     assignments.update(sort_date=order_product.order.planned_date)
 
     print('PASS')
     return f"Oki"
