83cd38b02a45fbeaba5639f4ab6dc825419ce085
bapyd <ba.pydev@gmail.com>
2026-01-23 15:55:47 +0300

    Фикс заказа

--- django_app/django_app/init_data/init_data.py
+++ django_app/django_app/init_data/init_data.py
@@ -1,7 +1,7 @@
 """Initial methods and scripts."""
 import logging
 
-# from core.models import OrderProduct, Assignment
+from core.models import OrderProduct, Assignment
 from django_app.init_data.reports.finance import get_finance_report
 
 logger = logging.getLogger(__name__)
@@ -11,13 +11,24 @@ def init_data():
     """Функция для активации скриптов через вызов url /init"""
     print('ИНИЦИАЛИЗАЦИЯ ФУНКЦИИ')
 
-    get_finance_report()
-    #
-    # active_order_products = OrderProduct.objects.filter(status="0")
-    #
-    # for order_product in active_order_products:
-    #     assignments = Assignment.objects.filter(order_product=order_product)
-    #     assignments.update(sort_date=order_product.order.planned_date)
+    op_from_series_id = "1-33346"
+    op_to_series_id = "3-33346"
+
+    op_from = OrderProduct.objects.get(series_id=op_from_series_id)
+    op_to = OrderProduct.objects.get(series_id=op_to_series_id)
+
+    target_assignments = Assignment.objects.filter(
+        order_product=op_from,
+        number__gte=3
+    )
+
+    for assignment in target_assignments:
+        assignment.number += 9
+        assignment.order_product = op_to
+        assignment.save()
+
+
+    # get_finance_report()
 
     print('PASS')
     return f"Oki"
